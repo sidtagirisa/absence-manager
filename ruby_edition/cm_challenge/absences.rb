bd3c2ba921ede071d6dbf2bbd19b3475e5ab38e8
Siddhardha Tagirisa <siddhardhatagirisa@gmail.com>
2020-05-06 12:31:54 +0800

    remove ruby edition

--- ruby_edition/cm_challenge/absences.rb
+++
@@ -1,9 +0,0 @@
-require_relative './api'
-
-module CmChallenge
-  class Absences
-    def to_ical
-      # Your implementation here
-    end
-  end
-end
--- ruby_edition/spec/absences_spec.rb
+++
@@ -1,5 +0,0 @@
-require_relative '../cm_challenge/absences'
-
-RSpec.describe 'Absences' do
-  it { skip 'Your tests here' }
-end
